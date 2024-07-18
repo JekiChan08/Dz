4e049031077dc7e94efd977db8b22e12154c1c50
mistergeki07 <mistergeki07@gmail.com>
2024-07-18 22:39:34 +0600

    Дз

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         TheCondidant theCondidant1 = new TheCondidant(20, 4, "Jeki", 200000);
         TheCondidant theCondidant2 = new TheCondidant(15, 1, "Jeki2", 400000);
+        TheCondidant theCondidant3 = new TheCondidant(15, 1, "Jeki2", 400000);
 
         Company company = new Company(19, 3, 300000);
         company.specifications(theCondidant1);
